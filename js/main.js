8dd56c5001ebdf88baf688258d7988a80a4780f1
Greg Inman <ginman86@gmail.com>
2013-08-31 22:37:55 -0500

    essays page initial wiring
    wired up essays page. it now works when fired off the "essays" footer
    action.

--- js/main.js
+++ js/main.js
@@ -1,54 +1,92 @@
-var programs, feed;
 
-$(document).ready(function () {
-	
-	loadPrograms();
-	
+var viewModels =
+{
+	programs: null,
+	essays:   null
+};
+
+$(document).ready(function () {	
+	access.loadPrograms();
+
 	initFooter();
+
+	$(".essayBtn").on
+	('click', 
+	function(e)
+	{
+		nav.essays();
+	});
 
 	var header = $("#header-main").html();
 	$("[id^=header]").empty().append(header);
 	$("div[data-role='page']").trigger('create');
-	
-	$(".summary").on
-	("click", function(e) 
-	{
-		ko.contextFor(this);
-		//loadSummary(this.)
-	});	
+
 });
 
-function loadPrograms()
-{		
-	var self = this;
-	
-	$.getJSON
-	(
-		"http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" +
-		"http://feeds.feedburner.com/ReformedForum?format=xml"  + 
-	  	"&callback=?", 
-		null, 
-		function(response)
-		{							
-			if (response && response.responseData && response.responseData.feed) 
-			{				
-				feed = ko.mapping.fromJS(response.responseData.feed);
-				ko.applyBindings
-				(
-					{
-						entries: 		feed.entries,
-		                arCategories:   
-		                function(categories, data)
-		                {
-		                    console.log(data);
-		                }    					
-					}
-				);
-				$('#programs').trigger('create');	
-			}
+var nav =
+{
+	essays:
+	function()
+	{
+		if (viewModels.essays == null)
+		{
+			access.loadPosts();
 		}
-	);
-}
+		else
+		{
+			ko.applyBindings(viewModels.essays, $('#essays').get(0));
+		}
+	}
+};
+
+var access = 
+{
+	loadPrograms:
+	function ()
+	{			
+		var feed;
+		$.getJSON
+		(
+			"http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" +
+			"http://feeds.feedburner.com/ReformedForum?format=xml"  + 
+		  	"&callback=?", 
+			null, 
+			function(response)
+			{							
+				if (response && response.responseData && response.responseData.feed) 
+				{	
+					feed = ko.mapping.fromJS(response.responseData.feed);
+					viewModels.programs = feed.entries;
+					ko.applyBindings(viewModels.programs(), $('#programs').get(0));
+					$('#programs').trigger('create');	
+				}
+			}
+		);
+	},
+	loadPosts:
+	function ()
+	{			
+		var feed;
+		$.getJSON
+		(
+			"http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" +
+			"http://reformedforum.org/feed/?post_type=post"  + 
+		  	"&callback=?", 
+			null, 
+			function(response)
+			{							
+				if (response && response.responseData && response.responseData.feed) 
+				{				
+					feed = ko.mapping.fromJS(response.responseData.feed);
+					viewModels.essays = feed.entries;
+					ko.applyBindings(viewModels.essays, $('#essays').get(0));
+					$('#essays').trigger('create');	
+				}
+			}
+		);
+	},
+};
+
 function initFooter()
 {		
 	var footer = $("#footer-main").html();
